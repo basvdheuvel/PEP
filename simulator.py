83064286ab4f8b3751b6c7e5a246e582c3e435b1
Bas van den Heuvel <bas.vandenheuvel@student.uva.nl>
2016-05-05 18:33:17 +0200

    Add docstrings for sim module and MachineControl

--- simulator.py
+++ simulator.py
@@ -1,10 +1,51 @@
+"""Simulates a purely event-driven programming language.
+
+This simulator is part of a computer science bachelor's thesis for the
+University of Amsterdam, by Bas van den Heuvel. It follows all concepts
+introduced in this thesis. The purpose is to be able to test and refine those
+concepts.
+
+The scheduling method is deterministicly sequential. No concurrent execution is
+implemented.
+
+Classes:
+MachineControl -- manages and schedules state machines and events
+Event -- event for communication between state machines
+StateMachine -- superclass for all possible state machines
+"""
 from collections import deque as queue
-# TODO: 'log' function wrapper.
 
 
 class MachineControl:
+    """Manage and schedule state machines and events.
+
+    Every program created with this simulator should have an instance of this
+    class. Starting a program goes through this instance, as well as
+    instantiating state machines and sending events.
+
+    Execution of its machine's states happens through cycles. The scheduling
+    for this is sequential and very minimalistic: the first machine in its
+    queue gets cycled after which it gets replaced at the end of the queue.
+
+    It can be said that there is no event scheduling. Before each state cycle,
+    all events in the event buss are distributed to their respective state
+    machines.
+    """
+
     def __init__(self, debug=False):
-        # Implementation choice: simple queued execution.
+        """Initialize a machine control.
+
+        It setups up a machine list, which in this implementation is a queue.
+        Reaction maps are created as dictionaries, and the event buss is
+        another queue.
+
+        The `ctx` variable is not yet set. This happens when the simulator is
+        started.
+
+        Keyword arguments:
+        debug -- prints state machine and event buss information if True
+            (default True)
+        """
         self.machines = queue()
 
         self.event_reactions = {}
@@ -17,6 +58,18 @@ class MachineControl:
         self.debug = debug
 
     def start_machine(self, machine_cls, ctx, *args, **kwargs):
+        """Start a state machine.
+
+        Initializes a machine, given arbitrary arguments, and adds it to the
+        machine queue. After this, some event reactions are added for this
+        machine, the `start' and the `halt' events. Finally, a `start' event is
+        emitted to the newly created machine.
+
+        Arguments:
+        machine_cls -- a StateMachine subclass
+        ctx -- the state machine that starts this new machine
+        *args/**kwargs -- any arguments the state machine takes
+        """
         machine = machine_cls(self, ctx, *args, **kwargs)
 
         self.machines.append(machine)
@@ -29,18 +82,39 @@ class MachineControl:
         return machine
 
     def add_event_reaction(self, typ, reactor, state):
+        """Add a reaction to an event.
+
+        Arguments:
+        typ -- the event's type string
+        reactor -- the machine that should react
+        state -- the state the machine should transition to
+        """
         if typ not in self.event_reactions:
             self.event_reactions[typ] = {}
 
         self.event_reactions[typ][reactor] = state
 
     def remove_event_reaction(self, typ, reactor):
+        """Remove a reaction to an event.
+
+        Arguments:
+        typ -- the event's type string
+        reactor -- the machine that should ignore the event
+        """
         try:
             del self.event_reactions[typ][reactor]
         except KeyError:
             pass
 
     def add_machine_reaction(self, typ, emitter, reactor, state):
+        """Add a reaction to a state machine's event.
+
+        Arguments:
+        typ -- the event's type string
+        emitter -- the event's emitting state machine
+        reactor -- the machine that should react
+        state -- the state the machine should transition to
+        """
         index = (typ, emitter)
 
         if index not in self.machine_reactions:
@@ -49,6 +123,13 @@ class MachineControl:
         self.machine_reactions[index][reactor] = state
 
     def remove_machine_reaction(self, typ, emitter, reactor):
+        """Remove a reaction to a state machine's event.
+
+        Arguments:
+        typ -- the event's type string
+        emitter -- the event's emitting state machine
+        reactor -- the machine that should ignore the event
+        """
         index = (typ, emitter)
 
         try:
@@ -57,9 +138,23 @@ class MachineControl:
             pass
 
     def emit(self, event):
+        """Add an event to the event buss.
+
+        Arguments:
+        event -- an event
+        """
         self.event_buss.append(event)
 
     def run(self, machine_cls, *args, **kwargs):
+        """Start a state machine and cycle until all machines have halted.
+
+        A context is created for this first machine, by instantiating the
+        StateMachine superclass without a context.
+
+        Arguments:
+        machine_cls -- a StateMachine subclass
+        *args/**kwargs -- any arguments the state machine takes
+        """
         self.ctx = StateMachine(self, None)
         self.start_machine(machine_cls, self.ctx, *args, **kwargs)
 
@@ -67,6 +162,11 @@ class MachineControl:
             pass
 
     def cycle(self):
+        """Distribute events, cycle a machine and return whether any are left.
+
+        When the machine queue is empty, False is returned. Otherwise, True is
+        returned.
+        """
         while self.distribute_events():
             pass
 
@@ -84,6 +184,16 @@ class MachineControl:
         return True
 
     def distribute_events(self):
+        """Distribute an event to machines and return whether any are left.
+
+        If an event has a destination and that destination is still alive (i.e.
+        not halted), the event is put into that machine's inbox.  Otherwise,
+        the event is put into the inbox of all live machines, except the
+        event's emitter.
+
+        If an event has been distributed, True is returned. Otherwise, False is
+        returned.
+        """
         try:
             event = self.event_buss.popleft()
         except IndexError:
@@ -105,6 +215,15 @@ class MachineControl:
         return True
 
     def filter_event(self, machine, event):
+        """Returns a state if a machine should react to an event.
+
+        If a reaction exists, the machine's reaction state is returned.
+        Otherwise, None is returned.
+
+        Arguments:
+        machine -- the reacting state machine
+        event -- the event to be checked
+        """
         if event.typ in self.event_reactions:
             try:
                 return self.event_reactions[event.typ][machine]
@@ -121,6 +240,7 @@ class MachineControl:
         return None
 
     def halt(self, machine):
+        """Halt a machine."""
         self.machines.remove(machine)
 
 
